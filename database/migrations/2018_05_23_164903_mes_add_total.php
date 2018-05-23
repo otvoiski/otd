cf5d5a27fdf67812131445693cadaa7d53de671f
Otávio Voiski <otvoiski@live.com>
2018-05-23 13:51:46 -0300

    Alteração na tabela feita com sucesso!

---
+++ database/migrations/2018_05_23_164903_mes_add_total.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class MesAddTotal extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('mes',function ($table){
+            $table->decimal('total',8,2);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
